3da24b9bbc87f530cbf679055c9418777d19eff1
Lab103 <Lab103@LAB101-07>
2017-05-05 16:22:57 -0300

    Manipulacao de Datas

---
+++ sceweb/teste/br/sceweb/teste/UC05CadastrarConvenio.java
@@ -0,0 +1,39 @@
+package br.sceweb.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.sceweb.modelo.Convenio;
+import br.sceweb.modelo.ConvenioDAO;
+
+public class UC05CadastrarConvenio {
+	static Convenio convenio;
+	static ConvenioDAO convenioDAO;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		convenioDAO = new ConvenioDAO();
+		convenio = new Convenio() ;
+	}
+	@Test
+	public void CT01UC05CadastrarConvenio_com_sucesso() {
+	assertEquals(1,convenioDAO.adiciona(convenio));
+	}
+	
+	@Test
+	public void CT03UC05A2Cadastrar_convenio_dti_invalida(){
+	assertFalse(convenio.validaData("42/05/2016"));
+	}
+	
+	@Test
+	public void CT02UC05A2Cadastrar_convenio_dti_valida(){
+	assertTrue(convenio.validaData("05/05/2016"));
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+}
